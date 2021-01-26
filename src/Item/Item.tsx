4db241152c3c14fa83ee6ec42d03670197ad0967
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-26 20:54:24 +0530

    fixed Button component in Item.tsx

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -19,7 +19,7 @@ const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
             <p>{item.description}</p>
             <h3>${item.price}</h3>
         </div>
-        <Button onClick={() => handleAddToCart(item)} />
+        <Button onClick={() => handleAddToCart(item)} >Add to Cart</Button>
     </Wrapper>
 )
 
