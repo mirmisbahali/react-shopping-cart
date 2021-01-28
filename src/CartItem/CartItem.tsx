06907d41a58b2ca26299f22e32aa6c928656d6cf
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-28 21:03:06 +0530

    edited CartItem component

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -6,7 +6,19 @@ import { CartItemType } from '../App';
 // Styles
 import { Wrapper } from './CartItem.styles';
 
-const CartItem: React.FC = () => <div>Cart Item</div>;
+type Props = {
+    item: CartItemType;
+    addToCart:(clicked: CartItemType) => void;
+    removeFromCart: (id: number) => void;
+}
+
+const CartItem: React.FC<Props> = ({item,  addToCart, removeFromCart}) => (
+    <Wrapper>
+        <div>
+            <h3>{Item.title}</h3>
+        </div>
+    </Wrapper>
+);
 
 export default CartItem;
 
